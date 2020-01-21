38a1a495e03007c45c51c1b4472e4326b7cf3a09
meet-panchal <connect.meetpanchal@gmail.com>
2020-01-21 19:10:19 +0530

    Design Changes and Bug Fixes

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -15,6 +15,7 @@ import {
   MDBModalFooter
 } from "mdbreact";
 import { Dropdown } from "react-bootstrap";
+import Swal from "sweetalert2";
 
 export default class PlayerInfo extends Component {
   state = {
@@ -66,7 +67,7 @@ export default class PlayerInfo extends Component {
     let updatedState = this.state.data.slice();
     let type = event.target.attributes.function.value;
     updatedState.forEach(player => {
-      if (player._id === playerDetail._id) {
+      if (player._id === playerDetail._id && !player.isPlayerLocked) {
         if (type === "increase") {
           player.basePrice += 10;
         } else {
@@ -86,211 +87,241 @@ export default class PlayerInfo extends Component {
       tempTeam: event.target.text
     });
   };
-  onSelect = event => {
-    alert(event.target);
+  alreadyLock = () => {
+    if (this.state.data[this.state.counter].isPlayerLocked) {
+      let timerInterval;
+      Swal.fire({
+        title: "Player is already locked!!",
+        html: "Look out for next available player",
+        timer: 2500,
+        timerProgressBar: true,
+        onBeforeOpen: () => {
+          Swal.showLoading();
+          /*           timerInterval = setInterval(() => {
+            Swal.getContent().querySelector(
+              "p"
+            ).textContent = Swal.getTimerLeft();
+          }, 100); */
+        },
+        onClose: () => {
+          clearInterval(timerInterval);
+        }
+      }).then(result => {
+        if (
+          /* Read more about handling dismissals below */
+          result.dismiss === Swal.DismissReason.timer
+        ) {
+          console.log("I was closed by the timer"); // eslint-disable-line
+        }
+      });
+    }
   };
   render() {
     return (
-      <div className="background">
-        <div className="custContainer">
-          <div className="row align-items-center">
-            <div className="col-md-1">
-              <FontAwesomeIcon
-                icon={faChevronLeft}
-                onClick={this.previousPlayer}
-                className="pointer"
-              />
-            </div>
-            <div className="col-md-4">
-              <div className="d-flex justify-content-between flex-column">
-                <div className="pb-4">
-                  <img
-                    src={require("../images/profile_pic.jpg")}
-                    alt=""
-                    className="customImage"
-                  />
-                </div>
-                <div className="d-flex justify-content-between">
-                  <div role="group" className="btn-group">
-                    <button
-                      type="button"
-                      className="btn btn-primary"
-                      function="decrease"
-                      onClick={event =>
-                        this.changeBasePrice(
-                          event,
-                          this.state.data[this.state.counter]
-                        )
-                      }
-                    >
-                      -
-                    </button>
-                    <button type="button" className="btn btn-outline-primary">
-                      {this.state.data[this.state.counter].basePrice}
-                    </button>
-                    <button
-                      type="button"
-                      function="increase"
-                      className="btn btn-primary"
-                      onClick={event =>
-                        this.changeBasePrice(
-                          event,
-                          this.state.data[this.state.counter]
-                        )
-                      }
-                    >
-                      +
-                    </button>
-                  </div>
-                  <div>
-                    <button
-                      className="btn btn-primary"
-                      onClick={this.toggle}
-                      disabled={
-                        this.state.data[this.state.counter].isPlayerLocked
-                      }
-                    >
-                      {this.state.data[this.state.counter].isPlayerLocked
-                        ? "Sold"
-                        : "Lock"}
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <table
-                className="table table-striped align-self-stretch"
-                style={{ width: "100%" }}
-              >
-                <tbody>
-                  <tr>
-                    <th>Name</th>
-                    <td>{this.state.data[this.state.counter].name}</td>
-                  </tr>
-                  <tr>
-                    <th>Age</th>
-                    <td>{this.state.data[this.state.counter].age}</td>
-                  </tr>
-                  <tr>
-                    <th>Type</th>
-                    <td>{this.state.data[this.state.counter].type}</td>
-                  </tr>
-                  <tr>
-                    <th>Base Price</th>
-                    <td>{this.state.data[this.state.counter].basePrice}</td>
-                  </tr>
-                  {this.state.data[this.state.counter].isPlayerLocked ? (
-                    <tr>
-                      <th>Team</th>
-                      <td>
-                        {this.state.data[this.state.counter].teamSelected}
-                      </td>
-                    </tr>
-                  ) : null}
-                </tbody>
-              </table>
-            </div>
-            <div className="col-md-1 text-right">
-              <FontAwesomeIcon
-                icon={faChevronRight}
-                onClick={this.nextPlayer}
-                className="pointer"
-              />
-            </div>
+      <div className="custContainer">
+        <div className="row align-items-center">
+          <div className="col-md-1">
+            <FontAwesomeIcon
+              icon={faChevronLeft}
+              onClick={this.previousPlayer}
+              className="pointer"
+            />
           </div>
-          <div className="row mt-5">
-            <div className="col-md-4">
-              <div className="card" style={{ maxWidth: "18rem" }}>
-                <div>
-                  <img
-                    src={require("../images/profile_pic.jpg")}
-                    alt=""
-                    className="customImage"
-                  />
-                </div>
-                <div className="card-body text-info text-center">
-                  <h5>{this.state.data[this.state.counter + 1].name}</h5>
-                </div>
+          <div className="col-md-4">
+            <div className="d-flex justify-content-between flex-column">
+              <div className="pb-4">
+                <img
+                  src={require("../images/profile_pic.jpg")}
+                  alt=""
+                  className="customImage"
+                />
               </div>
-            </div>
-            <div className="col-md-4">
-              <div className="card" style={{ maxWidth: "18rem" }}>
-                <div>
-                  <img
-                    src={require("../images/profile_pic.jpg")}
-                    alt=""
-                    className="customImage"
-                  />
-                </div>
-                <div className="card-body text-info text-center">
-                  <h5>{this.state.data[this.state.counter + 2].name}</h5>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="card" style={{ maxWidth: "18rem" }}>
-                <div>
-                  <img
-                    src={require("../images/profile_pic.jpg")}
-                    alt=""
-                    className="customImage"
-                  />
-                </div>
-                <div className="card-body text-info text-center">
-                  <h5>{this.state.data[this.state.counter + 3].name}</h5>
-                </div>
-              </div>
-            </div>
-          </div>
-          {this.state.modal ? (
-            <MDBContainer>
-              {/* <MDBBtn onClick={this.toggle}>Modal</MDBBtn> */}
-              <MDBModal isOpen={this.state.modal} toggle={this.toggle}>
-                <MDBModalHeader toggle={this.toggle}>
-                  Select Team
-                </MDBModalHeader>
-                <MDBModalBody>
-                  <Dropdown>
-                    <Dropdown.Toggle variant="info" id="dropdown-basic">
-                      {this.state.tempTeam
-                        ? this.state.tempTeam
-                        : "Available Teams"}
-                    </Dropdown.Toggle>
-
-                    <Dropdown.Menu onSelect={this.onSelect}>
-                      <Dropdown.Item onClick={this.clickHandler}>
-                        Mumbai Mavericks
-                      </Dropdown.Item>
-                      <Dropdown.Item onClick={this.clickHandler}>
-                        Haryana Hurricanes
-                      </Dropdown.Item>
-                      <Dropdown.Item onClick={this.clickHandler}>
-                        Deccan Chargers
-                      </Dropdown.Item>
-                      <Dropdown.Item onClick={this.clickHandler}>
-                        Garvi Gujarat
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </MDBModalBody>
-                <MDBModalFooter>
-                  <MDBBtn color="secondary" onClick={this.toggle}>
-                    Close
-                  </MDBBtn>
-                  <MDBBtn
-                    color="primary"
-                    onClick={() =>
-                      this.saveTeam(this.state.data[this.state.counter])
+              <div className="d-flex justify-content-between">
+                <div role="group" className="btn-group">
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    function="decrease"
+                    onClick={event =>
+                      this.changeBasePrice(
+                        event,
+                        this.state.data[this.state.counter]
+                      )
+                    }
+                    disabled={
+                      this.state.data[this.state.counter].isPlayerLocked
                     }
                   >
-                    Save changes
-                  </MDBBtn>
-                </MDBModalFooter>
-              </MDBModal>
-            </MDBContainer>
-          ) : null}
+                    -
+                  </button>
+                  <button
+                    type="button"
+                    className="btn btn-outline-primary"
+                    onClick={this.alreadyLock}
+                  >
+                    {this.state.data[this.state.counter].basePrice}
+                  </button>
+                  <button
+                    type="button"
+                    function="increase"
+                    className="btn btn-primary"
+                    onClick={event =>
+                      this.changeBasePrice(
+                        event,
+                        this.state.data[this.state.counter]
+                      )
+                    }
+                    disabled={
+                      this.state.data[this.state.counter].isPlayerLocked
+                    }
+                  >
+                    +
+                  </button>
+                </div>
+                <div>
+                  <button
+                    className="btn btn-primary"
+                    onClick={this.toggle}
+                    disabled={
+                      this.state.data[this.state.counter].isPlayerLocked
+                    }
+                  >
+                    {this.state.data[this.state.counter].isPlayerLocked
+                      ? "Sold"
+                      : "Lock"}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <table
+              className="table table-striped align-self-stretch"
+              style={{ width: "100%" }}
+            >
+              <tbody>
+                <tr>
+                  <th>Name</th>
+                  <td>{this.state.data[this.state.counter].name}</td>
+                </tr>
+                <tr>
+                  <th>Age</th>
+                  <td>{this.state.data[this.state.counter].age}</td>
+                </tr>
+                <tr>
+                  <th>Type</th>
+                  <td>{this.state.data[this.state.counter].type}</td>
+                </tr>
+                <tr>
+                  <th>Base Price</th>
+                  <td>{this.state.data[this.state.counter].basePrice}</td>
+                </tr>
+                {this.state.data[this.state.counter].isPlayerLocked ? (
+                  <tr>
+                    <th>Team</th>
+                    <td>{this.state.data[this.state.counter].teamSelected}</td>
+                  </tr>
+                ) : null}
+              </tbody>
+            </table>
+          </div>
+          <div className="col-md-1 text-right">
+            <FontAwesomeIcon
+              icon={faChevronRight}
+              onClick={this.nextPlayer}
+              className="pointer"
+            />
+          </div>
         </div>
+        <div className="row mt-5">
+          <div className="col-md-4">
+            <div className="card" style={{ maxWidth: "100%" }}>
+              <div>
+                <img
+                  src={require("../images/profile_pic.jpg")}
+                  alt=""
+                  className="customImage"
+                />
+              </div>
+              <div className="card-body text-info text-center">
+                <h5>{this.state.data[this.state.counter + 1].name}</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card" style={{ maxWidth: "100%" }}>
+              <div>
+                <img
+                  src={require("../images/profile_pic.jpg")}
+                  alt=""
+                  className="customImage"
+                />
+              </div>
+              <div className="card-body text-info text-center">
+                <h5>{this.state.data[this.state.counter + 2].name}</h5>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card" style={{ maxWidth: "100%" }}>
+              <div>
+                <img
+                  src={require("../images/profile_pic.jpg")}
+                  alt=""
+                  className="customImage"
+                />
+              </div>
+              <div className="card-body text-info text-center">
+                <h5>{this.state.data[this.state.counter + 3].name}</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+        {this.state.modal ? (
+          <MDBContainer>
+            {/* <MDBBtn onClick={this.toggle}>Modal</MDBBtn> */}
+            <MDBModal isOpen={this.state.modal} toggle={this.toggle}>
+              <MDBModalHeader toggle={this.toggle}>Select Team</MDBModalHeader>
+              <MDBModalBody>
+                <Dropdown>
+                  <Dropdown.Toggle variant="info" id="dropdown-basic">
+                    {this.state.tempTeam
+                      ? this.state.tempTeam
+                      : "Available Teams"}
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu onSelect={this.onSelect}>
+                    <Dropdown.Item onClick={this.clickHandler}>
+                      Mumbai Mavericks
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={this.clickHandler}>
+                      Haryana Hurricanes
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={this.clickHandler}>
+                      Deccan Chargers
+                    </Dropdown.Item>
+                    <Dropdown.Item onClick={this.clickHandler}>
+                      Garvi Gujarat
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </MDBModalBody>
+              <MDBModalFooter>
+                <MDBBtn color="secondary" onClick={this.toggle}>
+                  Close
+                </MDBBtn>
+                <MDBBtn
+                  color="primary"
+                  onClick={() =>
+                    this.saveTeam(this.state.data[this.state.counter])
+                  }
+                >
+                  Save changes
+                </MDBBtn>
+              </MDBModalFooter>
+            </MDBModal>
+          </MDBContainer>
+        ) : null}
       </div>
     );
   }
