bfd777b6e2789f4fc1d97cc57ac20f8542158bde
Meet Panchal <connect.meetpanchal@gmail.com>
2020-01-29 21:39:57 +0530

    Updates

---
+++ precache-manifest.5aaba770f201f4e158543d581acd9c9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120f714139997baa3c36d207d1c52e38",
+    "url": "/react-cricketAuction/index.html"
+  },
+  {
+    "revision": "2cdf42b7f4963d2957cb",
+    "url": "/react-cricketAuction/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cd3101ad61aad604314a",
+    "url": "/react-cricketAuction/static/css/main.c6df70c8.chunk.css"
+  },
+  {
+    "revision": "2cdf42b7f4963d2957cb",
+    "url": "/react-cricketAuction/static/js/2.c21e3d61.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/react-cricketAuction/static/js/2.c21e3d61.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cd3101ad61aad604314a",
+    "url": "/react-cricketAuction/static/js/main.4dab343c.chunk.js"
+  },
+  {
+    "revision": "92dcd44ba3528ac3ab84",
+    "url": "/react-cricketAuction/static/js/runtime-main.08b51305.js"
+  },
+  {
+    "revision": "2bce62de4f72307655635959f27eaad6",
+    "url": "/react-cricketAuction/static/media/profile_pic.2bce62de.jpg"
+  }
+]);
